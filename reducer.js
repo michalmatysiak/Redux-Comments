00eebbff79e49a32b8310790f9ce3d4963329c99
michalmatysiak <37140076+michalmatysiak@users.noreply.github.com>
2018-05-29 21:08:56 +0200

    Update reducer.js

--- reducer.js
+++ reducer.js
@@ -21,7 +21,7 @@ function comments(state = [], action) {
 		case REMOVE_COMMENT : 
 			return 	comments: state.filter(comment => comment.id !== action.id);
 		case EDIT_COMMENT: 
-			return comments: state.map(comment => (comment.id === action.id ? {..comment text: action.text} : comment));
+			return comments: state.map(comment => (comment.id === action.id ? {...comment text: action.text} : comment));
 		case THUMB_UP_COMMENT: 
 			return comments: state.map(comment => (comment.id === action.id ? {...comment, votes: comment.votes +1} : comment));
 		case THUMB_DOWN_COMMENT: 
