1afa90861b5ec468357d1535ce69a60470da23ad
michalmatysiak <37140076+michalmatysiak@users.noreply.github.com>
2018-05-31 16:35:06 +0200

    Update reducer.js

--- reducer.js
+++ reducer.js
@@ -1,8 +1,8 @@
-import { ADD_COMMENT } from './actions'
-import { REMOVE_COMMENT } from './actions'
-import { EDIT_COMMENT } from './actions'
-import { THUMB_UP_COMMENT } from './actions'
-import { THUMB_DOWN_COMMENT } from './actions'
+import { ADD_COMMENT } from "./actions";
+import { REMOVE_COMMENT } from "./actions";
+import { EDIT_COMMENT } from "./actions";
+import { THUMB_UP_COMMENT } from "./actions";
+import { THUMB_DOWN_COMMENT } from "./actions";
 
 const initialState = {
 	comments: [],
@@ -10,27 +10,40 @@ const initialState = {
 };
 
 function comments(state = [], action) {
-	switch(action.type) {
-		case ADD_COMMENT: 
-			return [{
-				id: action.id,
-				text: action.text,
-				votes: 0
-			},
-			...state];
-		case REMOVE_COMMENT : 
-			return 	comments: state.filter(comment => comment.id !== action.id);
-		case EDIT_COMMENT: 
-			return comments: state.map(comment => (comment.id === action.id ? {...comment text: action.text} : comment));
-		case THUMB_UP_COMMENT: 
-			return comments: state.map(comment => (comment.id === action.id ? {...comment, votes: comment.votes +1} : comment));
-		case THUMB_DOWN_COMMENT: 
-			return comments: state.map(comment => (comment.id === action.id ? {...comment, votes: comment.votes -1} : comment));
-		default: 
+	switch (action.type) {
+		case ADD_COMMENT:
+			return [
+				{
+					id: action.id,
+					text: action.text,
+					votes: 0
+				},
+				...state
+			];
+		case REMOVE_COMMENT:
+			return state.filter(comment => comment.id !== action.id);
+		case EDIT_COMMENT:
+			return state.map(
+				comment =>
+					comment.id === action.id
+						? { ...comment, text: action.text }
+						: comment
+			);
+		case THUMB_UP_COMMENT:
+			return state.map(
+				comment =>
+					comment.id === action.id
+						? { ...comment, votes: comment.votes + 1 }
+						: comment
+			);
+		case THUMB_DOWN_COMMENT:
+			return state.map(
+				comment =>
+					comment.id === action.id
+						? { ...comment, votes: comment.votes - 1 }
+						: comment
+			);
+		default:
 			return state;
 	}
 }
-
-
-
-
